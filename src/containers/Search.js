45ecd9cb4e8928bd148f1198f3f217cfa0e3b22c
Daniel Owens <DanielOwens@Daniel-Owenss-MacBook-Pro-2.local>
2019-08-15 19:53:56 -0700

    deleted material ui stuff

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import SearchBar from 'material-ui-search-bar';
-
 
 class Search extends React.Component {
   render() {
-  return (
-    <SearchBar
-      onChange={() => console.log('onChange')}
-      onRequestSearch={() => console.log('onRequestSearch')}
-      style={{
-        margin: '0 auto',
-        maxWidth: 800
-      }}
-    />
-  );
-}
+    return (
+      <div>
+        
+      </div>
+    );
+  } 
 }
 
 export default Search;
